9ea6dd4081683d4e9380dae913626311cb0eeb38
Laurent Eschenauer <laurent@eschenauer.be>
2013-03-26 21:33:15 +0100

    Renamed view to avoid confusion

--- protected/application/public/controllers/RepliesController.php
+++ protected/application/public/controllers/RepliesController.php
@@ -15,7 +15,7 @@
  *  limitations under the License.
  *  
  */
-class MentionsController extends BaseController
+class RepliesController extends BaseController
 {
 	
 	public function indexAction() {	
