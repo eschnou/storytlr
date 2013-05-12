284ecada5b611ddd38a142510147a22b06ee0e90
Laurent Eschenauer <laurent@eschenauer.be>
2013-05-12 10:18:14 +0200

    Fix lookup for entry author

--- protected/application/public/controllers/PingbackController.php
+++ protected/application/public/controllers/PingbackController.php
@@ -121,18 +121,20 @@ class PingbackController extends BaseController
 		if (!$user) {
 			throw new Exception('Failed to find corresponding storytlr user.'); 
 		}
-		
-		// Lookup author
-		if (count ($hcards) > 0) {
-			$hcard = $hcards[0];
-		}
-		
-		// Lookup entry
-		if (count ($hentries) > 0) {
+
+		// Is this a h-entry ?
+		if (count ($hentries) > 0) { 
 			$hentry = $hentries[0];
-			if (!$hcard && count($hentry["hcard"])>0) {
+			if (count($hentry["author"])>0) {
+				$hcard = $hentry["author"][0];
+			} else if (count($hentry["hcard"])>0) {
 				$hcard = $hentry["hcard"][0];
 			}
+		}
+		
+		// If no hcard yet (maybe there was no h-entry, we grab the top-level one
+		if (!$hcard && count ($hcards) > 0) {
+			$hcard = $hcards[0];
 		}
 		
 		// Add the mention to the database
@@ -245,6 +247,14 @@ class PingbackController extends BaseController
 		$hentry["title"] = $name ? $name : $content;
 		$hentry["published"] = @$props["published"][0];
 
+		if ($item["author"] && count($item["author"]) > 0) {
+			foreach ($item["author"] as $item) {
+				if (in_array("h-card", $item["type"])) {
+					array_push($hentry["author"], $this->processHCard($item));
+				}
+			}
+		} 
+		
 		if ($item["children"] && count($item["children"]) > 0) {
 			foreach ($item["children"] as $item) {
 				if (in_array("h-card", $item["type"])) {
